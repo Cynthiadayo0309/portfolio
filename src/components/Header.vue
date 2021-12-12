9c877a889469006a1d89d31fd3704097087e58d4
kosukeAoki <teyontt0309@gmail.com>
2021-12-12 21:52:21 +0900

    言語切り替え追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,31 @@
+<style lang="scss" scoped>
+@import "@/styles/header.scss";
+
+a {
+  text-decoration: none;
+}
+</style>
+
 <template>
   <div>
     <v-app-bar app color="#009DAE" class="logo">
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <v-toolbar-title style="margin-left: 9%">
-        <router-link to="/">AØKØU</router-link>
+        <router-link to="/">SØRAU</router-link>
       </v-toolbar-title>
       <v-tabs id="nav" color="#FFFFFF">
         <v-tab><router-link to="home">Home</router-link></v-tab>
         <v-tab><router-link to="about">About</router-link></v-tab>
         <v-tab><router-link to="skill">Skill</router-link></v-tab>
         <v-tab><router-link to="blog">Blog</router-link></v-tab>
-        <!-- ここに日本語と英語の切り替えを入れる -->
+        <v-tab>
+          <template v-if="$route.params.lang == 'ja'">
+            <router-link :to="en">En</router-link>
+          </template>
+          <template v-if="$route.params.lang == 'en'">
+            <router-link :to="ja">Jp</router-link>
+          </template>
+        </v-tab>
         <!-- 色々と追加する予定 -->
       </v-tabs>
     </v-app-bar>
@@ -37,7 +52,16 @@
               <router-link to="blog">Blog</router-link>
             </v-list-item-title>
           </v-list-item>
-          <!-- ここに日本語と英語の切り替えを入れる -->
+          <v-list-item>
+            <v-list-item-title>
+              <template v-if="$route.params.lang == 'ja'">
+                <router-link :to="en">日本語</router-link>
+              </template>
+              <template v-if="$route.params.lang == 'en'">
+                <router-link :to="ja">English</router-link>
+              </template>
+            </v-list-item-title>
+          </v-list-item>
           <!-- 色々と追加する予定 -->
         </v-list-item-group>
       </v-list>
@@ -52,40 +76,17 @@ export default {
       drawer: false,
     };
   },
+  computed: {
+    ja() {
+      let pathArray = this.$route.path.split('/')
+      pathArray[1] = 'ja'
+      return pathArray.join('/')
+    },
+    en() {
+      let path = this.$route.path.split('/')
+      path[1] = 'en'
+      return path.join('/')
+    },
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-@import "@/styles/header.scss";
-
-a {
-  text-decoration: none;
-}
-
-// .lang-box {
-//     position: absolute;
-//     bottom: 4px;
-//     right: 0;
-//     transform: translateY(100%);
-//     display: none;
-//     background: #fff;
-//     border: 1px solid #dedede;
-//     border-radius: 4px;
-//   }
-
-//   .lang-box li {
-//     margin: 0;
-//     font-size: 14px;
-//   }
-
-//   .lang-box li a {
-//     display: block;
-//     padding: 8px 16px;
-//     box-sizing: border-box;
-//     border-bottom: 1px dotted #adadad;
-//   }
-
-//   .lang-box li:last-child a {
-//     border-bottom: none;
-//   }
-</style>
