72aca5711bd06a2afa8357b017cdf7e45fc38215
Cynthiadayo0309 <teyontt0309@gmail.com>
2022-01-19 15:48:43 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,10 @@ module.exports = {
   chainWebpack: config => {
     config.module.rules.delete('eslint');
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/portfolio-app/'   //任意
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/portfolio-app/'   //任意
+  //   : '/'
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
 }
